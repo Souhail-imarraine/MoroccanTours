4b988724356d0611912be83c07c116d065292cef
imarrainesouhail202@gmail.com <imarrainesouhail202@gmail.com>
2026-03-04 11:24:09 +0000

    Add Moroccan tours application

---
+++ backend/src/main/java/com/moroccantour/MoroccanToursApplication.java
@@ -0,0 +1,13 @@
+package com.moroccantour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoroccanToursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoroccanToursApplication.class, args);
+	}
+
+}
