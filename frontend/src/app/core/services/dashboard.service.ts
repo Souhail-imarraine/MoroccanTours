96875f680ff667b263eabdedae3f84f70ef4804e
imarrainesouhail202@gmail.com <imarrainesouhail202@gmail.com>
2026-03-19 10:11:35 +0000

    Wire up Dashboard service

---
+++ frontend/src/app/core/services/dashboard.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { DashboardStatsResponse } from '../models/dashboard.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+  private apiUrl = `${environment.apiUrl}/dashboard`;
+
+  constructor(private http: HttpClient) {}
+
+  getGuideStats(): Observable<DashboardStatsResponse> {
+    return this.http.get<DashboardStatsResponse>(`${this.apiUrl}/guide`);
+  }
+
+  getAdminStats(): Observable<DashboardStatsResponse> {
+    return this.http.get<DashboardStatsResponse>(`${this.apiUrl}/admin`);
+  }
+}
